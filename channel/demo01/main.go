693f1d06c8861a3ca3d2253ec71347d0e6b162fb
lensxiong <363127224@qq.com>
2021-11-04 17:45:06 +0800

    add channel

--- channel/demo01/main.go
+++ channel/demo01/main.go
@@ -9,10 +9,10 @@ import (
 // 结果： 一段时间后总是输出 #goroutines: 2
 // 解析：因为 ch 未初始化，写和读都会阻塞，之后被第一个协程重新赋值，导致写的 ch 都阻塞。
 func main() {
-    var ch chan int
-    // ch = make(chan int, 1)
+    var ch chan int // nil
+    //ch := make(chan int, 1)
     go func() {
-        ch = make(chan int, 1)
+        //ch = make(chan int, 1)
         ch <- 1
     }()
     go func(ch chan int) {
@@ -24,6 +24,7 @@ func main() {
     //close(ch)
     c := time.Tick(1 * time.Second)
     for range c {
+        // NumGoroutine returns the number of goroutines that currently exist.
         fmt.Printf("#goroutines: %d\n", runtime.NumGoroutine())
     }
 }
