57ab18b24b2ce260c50628a74707dabd094db262
lensxiong <363127224@qq.com>
2021-11-28 22:01:55 +0800

    update map for range

--- map/for_range/main.go
+++ map/for_range/main.go
@@ -7,6 +7,16 @@ import "fmt"
 // wang => wang
 // zhou => wang
 
+// 逃逸分析 go build -gcflags '-m -l' ./main.go
+// # command-line-arguments
+// ./main.go:27:12: moved to heap: stu
+// ./main.go:17:14: make(map[string]*student) does not escape
+// ./main.go:20:26: []student literal does not escape
+// ./main.go:38:20: ... argument does not escape
+// ./main.go:38:20: k escapes to heap
+// ./main.go:38:24: "=>" escapes to heap
+// ./main.go:38:31: v.Name escapes to heap
+
 type student struct {
     Name string
     Age  int
@@ -17,17 +27,21 @@ func main() {
     m := make(map[string]*student)
 
     // 定义student数组
-    stus := []student{
+    students := []student{
         {Name: "zhou", Age: 24},
         {Name: "li", Age: 23},
         {Name: "wang", Age: 22},
     }
 
-    // 遍历结构体数组，依次赋值给map
-    for i := 0; i < len(stus); i++  {
-        m[stus[i].Name] = &stus[i]
+    // 将数组依次添加到map中（错误写法）
+    for _, stu := range students {
+        m[stu.Name] = &stu
     }
 
+    // 遍历结构体数组，依次赋值给map （正确写法）
+    // for i := 0; i < len(students); i++  {
+    //    m[students[i].Name] = &students[i]
+    // }
 
     // 打印map
     for k, v := range m {
