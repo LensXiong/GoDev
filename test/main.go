813343c36097cc277175682123d28df19c830784
lensxiong <363127224@qq.com>
2021-12-04 23:55:30 +0800

    add goroutine pool

--- test/main.go
+++ test/main.go
@@ -1,14 +1,60 @@
 package main
 
-import (
-    "fmt"
-)
+//func main() {
+//    arr := make([]int, 0)
+//    for i := 0; i < 2000; i++ {
+//        fmt.Println("len 为", len(arr), "cap 为", cap(arr))
+//        arr = append(arr, i)
+//    }
+//
+//}
 
-func main() {
-    arr := make([]int, 0)
-    for i := 0; i < 2000; i++ {
-        fmt.Println("len 为", len(arr), "cap 为", cap(arr))
-        arr = append(arr, i)
-    }
+//// 1 1 2 3 5 8 13 21 34 55
+//func main (){
+//    slice := make([]int,10)
+//    for i := 0; i< 10; i++{
+//        if i < 2{
+//            slice[i] =  1
+//        } else {
+//            slice[i] = slice[i-2] + slice[i-1]
+//        }
+//
+//    }
+//    fmt.Println("slice ",slice)
+//}
+
+
+/*
+[{
+	"name": "aaa",
+	"children": [{
+		"name": "bbb",
+		"children": []
+	}]
+}, {
+	"name": "ccc",
+	"children": []
+}]
+
+[{
+	"name": "aaa",
+	"children": []
+}, {
+	"name": "bbb",
+	"children": []
+}, {
+	"name": "ccc",
+	"children": []
+}]
+
+ */
+
+
+
+
+
+func main(){
+    slice := []int64{89,3,8,67,78,44,63,9}
 
 }
+
