02316912412741f2bb508ba6b2dd916818c70bfc
wangxiong <363127224@qq.com>
2021-12-29 10:01:46 +0800

    add defer painc recover 02

--- defer_painc_recover/main.go
+++ defer_painc_recover/main.go
@@ -8,8 +8,9 @@ import (
 // writeFile recover Err writeFile panic
 
 // 写文件
-func writeFile(firstTestScript string, wg *sync.WaitGroup) {
+func writeFile(wg *sync.WaitGroup) {
 	defer wg.Done()
+	// defer wg.Add(-1)
 	defer func() {
 		if recoverErr := recover(); recoverErr != nil {
 			fmt.Println("writeFile recover Err", recoverErr)
@@ -20,11 +21,10 @@ func writeFile(firstTestScript string, wg *sync.WaitGroup) {
 
 // 打包文件
 func main() {
-	testScriptSlice := []string{"111", "222"}
 	var wg sync.WaitGroup
-	for _, v := range testScriptSlice {
+	for i := 0; i < 50; i++ {
 		wg.Add(1)
-		go writeFile(v, &wg)
+		go writeFile(&wg)
 	}
 	wg.Wait()
 }
