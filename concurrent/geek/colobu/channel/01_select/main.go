b6c4a5a009b5e1b144c391e04ddebc8d90e505e9
wangxiong <363127224@qq.com>
2022-01-02 23:02:31 +0800

    add vim channel reflect select

---
+++ concurrent/geek/colobu/channel/01_select/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var ch = make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		select {
+		case ch <- i:
+
+		case v := <-ch:
+			fmt.Println(v)
+		}
+	}
+}
