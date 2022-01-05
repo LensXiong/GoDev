111cf4f4b5a6fb70a9ccd85ea44c286b65d90b14
wangxiong <lensxiong@gmail.com>
2022-01-05 20:45:07 +0800

    add etcd issue

--- concurrent/geek/colobu/channel/05_bug_etcd/main.go
+++ concurrent/geek/colobu/channel/05_bug_etcd/main.go
@@ -1,6 +1,8 @@
 package main
 
 // [etcd issue 6857](https://github.com/etcd-io/etcd/pull/6857/commits/7afc490c95789c408fbc256d8e790273d331c984)
+// [etcd issue 5505](https://github.com/etcd-io/etcd/pull/5505/commits/09e8f5782e91a3287c5f38b190f7b74e93bb2049)
+// [etcd issue 11256](https://github.com/etcd-io/etcd/issues/11256)
 
 // raft/node.go
 
